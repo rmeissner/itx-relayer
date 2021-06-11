c7cf0f0d90f9142912adcfc5fac1199500c6c91e
Richard Meissner <msc.meissner@gmail.com>
2021-06-11 12:57:08 +0200

    Add test for deposit via relay

--- test/GasTank.spec.ts
+++ test/GasTank.spec.ts
@@ -3,6 +3,7 @@ import hre, { deployments, waffle } from "hardhat";
 import "@nomiclabs/hardhat-ethers";
 import { _TypedDataEncoder } from "@ethersproject/hash";
 import { getConfig } from "../src/config/refunder_config";
+import { BigNumber, utils } from "ethers";
 
 describe("Refunder", async () => {
 
@@ -41,6 +42,20 @@ describe("Refunder", async () => {
             await expect(
                 executor.exec(gasTank.address, 0, calldata).then((tx:any) => { console.log({tx}); return tx})
             ).to.be.revertedWith("Not Authorized")
+        })
+    })
+
+    describe.only("deposit", async () => {
+        it("can deposit with relay", async () => {
+            const { gasTank, executor } = await setupTest();
+
+            await user1.sendTransaction({ to: executor.address, value: utils.parseEther("10") })
+    
+            const relayData = "0x" + executor.interface.encodeFunctionData("exec", [gasTank.address, utils.parseEther("1"), "0x"]).slice(10)
+            await gasTank.execute(executor.address, relayData)
+            const account = await gasTank.accounts(executor.address)
+            expect(account.deposit).to.be.equal(utils.parseEther("1"))
+            expect(account.used).to.be.gt(BigNumber.from(0))
         })
     })
 })
